1dce47619b4170344b19bb46c8c3e33d1dcb8700
Aleksey Sidorov <sauron1987@gmail.com>
2024-03-15 13:06:07 +0300

    Cleanup cask

--- Casks/foundationdb@7.1.rb
+++ Casks/foundationdb@7.1.rb
@@ -1,37 +1,43 @@
 cask "foundationdb@7.1" do
-    arch arm: "arm64" 
+  arch arm: "arm64" 
 
-    version "7.1.57"
-    sha256 "6265c4bae30c0b967225b7e61ed81a653e93282de687b3f7bf7ac314e20ab055"
-  
-    url "https://github.com/apple/foundationdb/releases/download/#{version}/FoundationDB-#{version}_#{arch}.pkg"
-    name "FoundationDB"
-    desc "The open source, distributed, transactional key-value store"
-    homepage "https://www.foundationdb.org"
-  
-    # livecheck do
-    #   url "https://zed.dev/releases/stable"
-    #   regex(%r{href=.*?/stable/(\d+(?:\.\d+)+)/Zed.dmg}i)
-    # end
-  
-    auto_updates false
-    depends_on macos: ">= :catalina"
-  
-    pkg "FoundationDB-#{version}_#{arch}.pkg"
+  version "7.1.57"
+  sha256 "6265c4bae30c0b967225b7e61ed81a653e93282de687b3f7bf7ac314e20ab055"
 
-    uninstall launchctl: [ ],
-      script:    {
-        executable:   "/usr/local/foundationdb/uninstall-FoundationDB.sh",
-        must_succeed: false, # A non-0 exit code may be given even if the uninstall succeeds (https://github.com/Homebrew/homebrew-cask/issues/21740#issuecomment-224094946).
-        sudo:         true,
+  url "https://github.com/apple/foundationdb/releases/download/#{version}/FoundationDB-#{version}_#{arch}.pkg"
+  name "FoundationDB"
+  desc "The open source, distributed, transactional key-value store"
+  homepage "https://www.foundationdb.org"
+
+  auto_updates false
+
+  pkg "FoundationDB-#{version}_#{arch}.pkg",
+    choices: [
+      {
+        "choiceIdentifier" => "clients",
+        "choiceAttribute"  => "selected",
+        "attributeSetting" => 1,
       },
-      pkgutil:   [
-        "FoundationDB-clients",
-        "FoundationDB-server"
-      ]
-  
-    zap trash: [
-      "/usr/local/foundationdb",
-      "/usr/local/etc/foundationdb"
+      {
+        "choiceIdentifier" => "server",
+        "choiceAttribute"  => "selected",
+        "attributeSetting" => 1,
+      },
     ]
+
+  uninstall launchctl: [ ],
+    script:    {
+      executable:   "/usr/local/foundationdb/uninstall-FoundationDB.sh",
+      must_succeed: false, # A non-0 exit code may be given even if the uninstall succeeds (https://github.com/Homebrew/homebrew-cask/issues/21740#issuecomment-224094946).
+      sudo:         true,
+    },
+    pkgutil:   [
+      "FoundationDB-clients",
+      "FoundationDB-server"
+    ]
+
+  zap trash: [
+    "/usr/local/foundationdb",
+    "/usr/local/etc/foundationdb"
+  ]
 end
